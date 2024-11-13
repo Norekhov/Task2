7de70dea7e2836da70ce0bba5fc634c43fc4004e
Norekhov <156809648+Norekhov@users.noreply.github.com>
2024-11-13 12:59:47 +0300

    Билет № 13

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 //Количество полок константное — 5 штук.
 //Вернуть книги, распределенные по полкам.
 
+
 public class Main {
     public static void main(String[] args) {
 
